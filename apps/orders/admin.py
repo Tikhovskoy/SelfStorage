0e5d8fe07771ac111bad378397a963831a6fc3ef
Aleksashka301 <aleksashka301@gmail.com>
2025-07-23 21:43:23 +0500

    Update admin.py

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+	list_display = ['first_name', 'last_name', 'phone', 'registered_at']
+	readonly_fields = ['registered_at']

447f5b46e9e2ad3021db31c0141c0847700faed5
Victor Tikhovskoi <tihovskoyviktor@icloud.com>
2025-07-28 19:10:11 +0700

    подключил тарифы к админке, пока как демонстрация

--- apps/storage_units/admin.py
+++ apps/storage_units/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
-from .models import Warehouse, Box
+from .models import Warehouse, Box, Tariff
 
 
 @admin.register(Warehouse)
 class WarehouseAdmin(admin.ModelAdmin):
-	list_display = ['name', 'address', 'temperature']
-	search_fields = ['name', 'address']
+    list_display = ['name', 'address', 'temperature']
+    search_fields = ['name', 'address']
 
 
 @admin.register(Box)
 class BoxAdmin(admin.ModelAdmin):
-	list_display = ['id', 'warehouse', 'floor', 'square', 'height', 'price', 'is_free']
-	list_filter = ['warehouse', 'square', 'height', 'is_free']
-	search_fields = ['id']
+    list_display = ['id', 'warehouse', 'floor', 'square', 'height', 'price', 'is_free']
+    list_filter = ['warehouse', 'square', 'height', 'is_free']
+    search_fields = ['id', 'tenant__user__email']
+    readonly_fields = ['square']
+
+
+@admin.register(Tariff)
+class TariffAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'min_square_meters', 'max_square_meters']
+    list_filter = ['min_square_meters']
+    search_fields = ['name']
